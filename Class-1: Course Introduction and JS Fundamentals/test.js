a9ed40e7170f212d79405042c28e8fd4e7a34893
hasnat_shohag <hasnat.ru.ice19@gmail.com>
2023-09-13 15:17:58 +0600

    js file update

---
+++ Class-1: Course Introduction and JS Fundamentals/test.js
@@ -0,0 +1,39 @@
+//                                      Arrow Function
+
+// const square =  (n) => n * n;
+// console.log(square(5));
+
+
+//                                      Object distructuring 
+
+// const Car = {
+//     cc : 500,
+//     color: "red", 
+//     price: "$5K"
+// };
+
+// const maruti = {...Car, miledge : 50}
+// let {cc,color ,price, miledge} = maruti;
+// console.log(maruti);
+
+
+//                                      map in Js
+
+// let arr = [1,2,4,5,6];
+// arr.map(element => document.writeln(element));
+
+
+
+//                                      method
+
+// const Car = {
+//     cc : 500,
+//     color: "red", 
+//     price: "$5K",
+//     start(){
+//         console.log("Started, drive the car safely");
+//     }
+// };
+
+// Car.start();
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+In this repository i track my learning and practice of Learnathon 2.0 at vivasoft
