dcc0e84534449dc7dea54b0d307f7131bf493b3a
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-09-24 17:14:01 +0600

    event bubbling and capturing

--- Class-3: DOM/script.js
+++ Class-3: DOM/script.js
@@ -46,3 +46,50 @@ console.log('It works!');
 
 // const list = document.querySelector("#myList");
 // list.firstElementChild.textContent = 'Typescript'; 
+
+// const newDiv = document.querySelector('div');
+// newDiv.className += 'hightlight';
+// newDiv.className += ' hightlight2';
+
+// const grandparent = document.getElementById("grandparent");
+// const parent = document.getElementById("parent"); 
+// const child = document.getElementById("child") 
+// // bubbling 
+// grandparent.addEventListener("click", ()=> { console.log("Grandparent clicked"); }, false) 
+// // capturing or trickling 
+// parent.addEventListener("click", (e)=> { 
+//     console.log("Parent clicked"); 
+//     e.stopPropagation();
+// }) 
+// // bubbling 
+// child.addEventListener("click", ()=> { console.log("Child clicked"); }, false)
+
+// const redDiv = document.querySelector('#red');
+// const greenDiv = document.querySelector('#green');
+// const skyblueDiv = document.querySelector('#skyblue');
+// const blueDiv = document.querySelector('#blue');
+// const purpleDiv = document.querySelector('#purple');
+// const goldDiv = document.querySelector('#gold');
+ 
+// // Event Bubbling Phase 
+// redDiv.addEventListener('click', () => {
+//     console.log('Red box clicked.');
+// }, false)
+// greenDiv.addEventListener('click', () => {
+//     console.log('Green box clicked');
+// }, false)
+// skyblueDiv.addEventListener('click', (e) => {
+    
+//     console.log('Skyblue box clicked');
+// }, false)
+ 
+// // Event Capturing Phase
+// blueDiv.addEventListener('click', () => {
+//     console.log('Blue box clicked.');
+// }, true)
+// purpleDiv.addEventListener('click', () => {
+//     console.log('Purple box clicked');
+// }, true)
+// goldDiv.addEventListener('click', () => {
+//     console.log('Golden box clicked');
+// }, true)
