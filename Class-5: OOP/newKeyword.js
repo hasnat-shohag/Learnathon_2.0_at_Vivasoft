56d1ee6dd7a5352c0530629e92e7d39b8568d498
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-10-01 16:21:15 +0600

    new keyword

---
+++ Class-5: OOP/newKeyword.js
@@ -0,0 +1,20 @@
+// new কিওয়ার্ড ব্যবহার না করে ফাংশনাল ও ক্লাস constructor এর মাধ্যমে অবজেক্ট ডিফাইন করার চেষ্টা করো 
+
+// Factory Pattern 
+// function er vitor 1ta new object define or create kore return korte hoy
+
+// function createPerson(name, age) {
+// 	return {
+// 		name,
+// 		age,
+// 		sayHello() {
+// 			console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+// 		},
+// 	};
+// }
+
+// const person = createPerson("Mr. X", 30);
+// person.sayHello();
+
+
+
