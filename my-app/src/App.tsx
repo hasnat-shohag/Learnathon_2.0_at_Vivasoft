dbb0cb9980bf56acf9c77987a1855583245f4f11
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-10-20 07:42:14 +0600

    button counter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,12 @@
+import { count } from "console";
 import "./App.css";
 import Button from "./button";
 
 function App() {
   return <div className="App">
-    <Button size = '20px'/>
+    <Button content = 'Increment'/>
+    
+    <Button content = 'Decrement'/>
   </div>;
 }
 
--- my-app/src/button/button.tsx
+++ my-app/src/button/button.tsx
@@ -1,15 +1,42 @@
-import './button.css';
+import { useState } from "react";
+import "./button.css";
 
-interface ButtonPropsTypes{
-    size: string;
+interface ButtonPropsTypes {
+    content: string;
 }
-// function btn(){
-//     console.log("CLicked");
-//     ++count;
+
+const Button = (props: ButtonPropsTypes) => {
+    let [count, setCount] = useState<number>(5);
+    const { content } = props;
+
+    return (
+        <button
+            onClick={() => {
+                content === "Increment"
+                    ? setCount(count + 1)
+                    : setCount(count - 1);
+            }}
+        >
+            {content} {count}
+        </button>
+    );
+    // <button className= '_style' onClick={(content === 'Increment' ? Increment : Decrement)}>{content} {count} </button>
+
+    //! How two buttons share same 'count' variable value (not forming closer for a specific variable)
+};
+// const Button = (props: ButtonPropsTypes)=>{
+//     const [count, setCount] = useState<number>(5);
+//     const {content} = props;
+
+//     const Increment =  () => {
+//         setCount(count+1);
+//     }
+//     const Decrement =  () => {
+//         (count-1 >= 0 ? setCount(count-1): alert("Negative Count Not allowed"));
+//     }
+
+//     return <button className= '_style' onClick={(content === 'Increment' ? Increment : Decrement)}>{content} {count} </button>
+
+//     //! How two buttons share same variable value (not forming closer for a specific variable)
 // }
-let count = 0;
-const Button = (props: ButtonPropsTypes)=>{
-    const {size} = props;
-    return <button className= '_style' >Click Me {count}</button>
-}
 export default Button;
