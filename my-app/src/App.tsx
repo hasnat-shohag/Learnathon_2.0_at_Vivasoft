1cfa5bad729276ea5cbce4e7c0a6613998158225
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-11-05 00:50:01 +0600

    Form Handle

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,15 +2,17 @@ import { count } from "console";
 import "./App.css";
 import Button from "./button";
 import Counter from "./counter/useReducer";
+import FormSubmit from "./Components/FormSubmit"; 
+
 
 function App() {
     return (
         <div className="App">
             {/* <Button content = 'Increment'/>
             <Button content = 'Decrement'/> */}
-            <Counter />
-            <Counter />
-            <Counter />
+            {/* <Counter />
+            <Counter /> */}
+            <FormSubmit />
         </div>
     );
 }
---
+++ my-app/src/Components/FormSubmit.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const FormSubmit = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const EmailChange = (event: { target: { value: any; }; }) =>{
+        setEmail(event.target.value);
+    }
+    const PasswordChange = (event: { target: { value: any; }; }) =>{
+        setPassword(event.target.value);
+    }
+    return (
+        <form onSubmit={(e) => {e.preventDefault(); console.log("Email:", email, " ","Password:",password);}}>
+            <p>
+                Email: <input type="text" name="email" onChange={EmailChange} />
+            </p>
+            <p>
+                Password: <input type="password" name="password" onChange={PasswordChange}/>
+            </p>
+            <button type="submit"> Submit </button>
+        </form>
+    );
+}
+
+export default FormSubmit;
